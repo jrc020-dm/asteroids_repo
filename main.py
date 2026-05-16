ac3d9c674c59b8f9426d7a2212f2b31f6cb63948
jrc020-dm <cummings.robert.james@gmail.com>
2026-05-16 15:41:54 -0400

    F: add left and right rotation to play loop

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
    
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player = Player(x = (SCREEN_WIDTH / 2), y = (SCREEN_HEIGHT / 2))
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +17,7 @@ def main():
                 return
         
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
